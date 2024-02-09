7b23024294a28aad5ef31d0c3268ee3d532340c5
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-02-09 01:01:20 -0500

    500px styling

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -15,3 +15,18 @@ customElements.whenDefined("small-header").then(() => {
     menu.style.animation = "slideOut 0.15s ease-in forwards"
   })
 })
+
+const dropdownContainer = document.querySelector(".services-dropdown-container")
+const dropdown = document.querySelector(".dropdown")
+const servicesLink = document.getElementById("services")
+
+dropdownContainer.addEventListener("mouseover", () => {
+  servicesLink.style.animation = "remainHoverStyle 0.15s ease-in forwards"
+  dropdown.style.display = "grid"
+  dropdown.style.animation = "showDropdown 0.15s ease-in forwards"
+})
+
+dropdownContainer.addEventListener("mouseleave", () => {
+  servicesLink.style.animation = "removeHoverStyle 0.15s ease-in forwards"
+  dropdown.style.animation = "hideDropdown 0.15s ease-in forwards"
+})
